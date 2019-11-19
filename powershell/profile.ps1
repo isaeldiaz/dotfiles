d388c3d44f1b0922fff9ef8e63bdc95bf4ec5acf
isdi <isael.diaz@nordicsemi.no>
2019-11-19 13:41:24 +0100

    including powershell profile script

---
+++ powershell/profile.ps1
@@ -0,0 +1,49 @@
+
+Set-Location $HOME
+
+Set-PSReadlineOption -EditMode vi
+
+# Dracula readline configuration. Requires version 2.0, if you have 1.2 convert to `Set-PSReadlineOption -TokenType`
+Set-PSReadlineOption -Color @{
+    "Command" = [ConsoleColor]::Green
+    "Parameter" = [ConsoleColor]::Gray
+    "Operator" = [ConsoleColor]::Magenta
+    "Variable" = [ConsoleColor]::White
+    "String" = [ConsoleColor]::Yellow
+    "Number" = [ConsoleColor]::Blue
+    "Type" = [ConsoleColor]::Cyan
+    "Comment" = [ConsoleColor]::DarkCyan
+}
+
+
+# Dracula Prompt Configuration
+Import-Module posh-git
+$GitPromptSettings.DefaultPromptPrefix.Text = "$([char]0x2192) " # arrow unicode symbol
+$GitPromptSettings.DefaultPromptPrefix.ForegroundColor = [ConsoleColor]::Green
+$GitPromptSettings.DefaultPromptPath.ForegroundColor =[ConsoleColor]::Cyan
+$GitPromptSettings.DefaultPromptSuffix.Text = "$([char]0x203A) " # chevron unicode symbol
+$GitPromptSettings.DefaultPromptSuffix.ForegroundColor = [ConsoleColor]::Magenta
+# Dracula Git Status Configuration
+$GitPromptSettings.BeforeStatus.ForegroundColor = [ConsoleColor]::Blue
+$GitPromptSettings.BranchColor.ForegroundColor = [ConsoleColor]::Blue
+$GitPromptSettings.AfterStatus.ForegroundColor = [ConsoleColor]::Blue
+
+
+########### HELPING FUNCTIONS ###############
+function Get-Docker-Volume-Path {
+  [CmdletBinding()]
+  param(
+      [Parameter()]
+      [string]$h,
+      [Parameter()]
+      [string]$g
+  )
+  if (-Not $h){
+    $h = (Get-Location).ToString()
+  }
+  if (-Not $g){
+    $g = "/workdir"
+  }
+  Write-Host ("-v {0}:{1}" -f $h.ToLower().replace("\","/"),$g.ToLower().replace("\","/"))
+
+}
