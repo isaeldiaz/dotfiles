c585a85e90080a05ca56760745d681e50fb184ea
Isael Diaz <isael.diaz@huawei.com>
2024-04-09 14:28:49 +0200

    fixing unpacking function for dotfiles

--- pack_dotfiles.sh
+++ pack_dotfiles.sh
@@ -2,17 +2,20 @@
 
 COMMAND=$1
 
+
 if [[ "$COMMAND" == "pack" ]]; then
   HOME_DIR="$HOME"
   DOTFILES_DIR="$HOME/dotfiles"
   PLUGIN_DIR="$HOME_DIR/.config/.vim/plugged"
-  VIM_INIT_DIR="$HOME_DIR/dotfiles/vim"
+  OHMYZSH_DIR="$HOME/.oh-my-zsh"
+
   cd /tmp
   mkdir dotfiles; cd dotfiles
   cp -R $PLUGIN_DIR .
   cp -R $DOTFILES_DIR .
+  cp -R $OHMYZSH_DIR .
   date > "sync_date.txt"
-  find . -name ".git" | xargs rm -Rf
+  find dotfiles -name ".git" | xargs rm -Rf
   cd ..
   tar -cvf $HOME/dotfiles.tar ./dotfiles
   rm -Rf /tmp/dotfiles
@@ -21,30 +24,47 @@ if [[ "$COMMAND" == "pack" ]]; then
 
 elif [[ "$COMMAND" == "unpack" ]]; then
 
-TARGET_OS=$2
+  if [[ -z $2 ]]; then
+    TARGET_OS="linux"
+  else
+    TARGET_OS=$2
+  fi
 
   # Linux
   if [[ -n "$WSL_DISTRO_NAME" ]]; then
+     export WIN_USER=$(powershell.exe '$env:USERNAME' | tr -d '\r')
      if [[ "$TARGET_OS" == "win" ]]; then
         # Windows dirs via WSL
-        HOME_DIR="/mnt/c/Users/$USER"
+        HOME_DIR="/mnt/c/Users/$WIN_USER"
         PLUGIN_DIR="$HOME_DIR/.config/nvim/plugged"
-        VIM_INIT_DIR="$HOME_DIR/AppData/Local/nvim"
+        DOTFILES_DIR="$HOME_DIR/dotfiles"
      else 
         #All linux distros
         HOME_DIR="$HOME"
         PLUGIN_DIR="$HOME_DIR/.config/.vim/plugged"
-        VIM_INIT_DIR="$HOME_DIR/dotfiles/vim"
+        DOTFILES_DIR="$HOME_DIR/dotfiles"
+        OHMYZSH_DIR="$HOME/.oh-my-zsh"
      fi
   fi
 
-  # Create the directory if it doesn't exist
-  mkdir -p $DIRECTORY
+  ls $HOME_DIR/scratch
 
-  # Navigate to the directory
-  cd $DIRECTORY
+  if [[ -f "$HOME_DIR/dotfiles.tar" ]]; then
+    pushd /tmp
+    tar -xvf "$HOME_DIR/dotfiles.tar" 
+    cd dotfiles
+    rsync -av plugged "$PLUGIN_DIR"
+    rsync -av dotfiles "$DOTFILES_DIR"
 
-  # Use the unzip command to extract the plugins
-  unzip path_to_your_zip_file/vim_plugins.zip
+    [[ -v OHMYZSH_DIR ]] && rsync -av .oh-my-zsh $OHMYZSH_DIR
+
+    cd ..
+    rm -Rf dotfiles
+    popd
+    echo "Unpacked completed"
+  else
+    echo "There is no tar file $HOME_DIR/dotfiles.tar"
+  fi
+
 
 fi
