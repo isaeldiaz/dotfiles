d360718d5b26270c0a6616df4929b4f57a31121b
Isael Diaz <Isael.Diaz@numascale.com>
2025-11-14 12:31:46 +0100

    smallfixes to lua nvim conf

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -13,7 +13,25 @@ return {
       vim.g.airline_theme = "deus"
       -- Enable powerline fonts if available
       vim.g.airline_powerline_fonts = 1
-    end,
+
+      -- Manually set Nerd Font symbols using vim.fn.nr2char
+      vim.cmd([[
+      if !exists('g:airline_symbols')
+        let g:airline_symbols = {}
+        endif
+
+        " Powerline symbols
+        let g:airline_left_sep = nr2char(0xe0b0)
+        let g:airline_left_alt_sep = nr2char(0xe0b1)
+        let g:airline_right_sep = nr2char(0xe0b2)
+        let g:airline_right_alt_sep = nr2char(0xe0b3)
+        let g:airline_symbols.branch = nr2char(0xe0a0)
+        let g:airline_symbols.readonly = nr2char(0xe0a2)
+        let g:airline_symbols.linenr = nr2char(0xe0a1)
+        let g:airline_symbols.maxlinenr = nr2char(0xe0a1)
+        let g:airline_symbols.dirty = nr2char(0x26a1)
+        ]])
+      end,
   },
 
   -- Show marks in sign column
