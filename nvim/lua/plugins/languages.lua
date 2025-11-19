415ebf37c7f8d886936387e3f5f4a06fa8da9a9c
Isael Diaz <Isael.Diaz@numascale.com>
2025-11-19 09:22:30 +0100

    fixing missing render-markdown dependencies

--- nvim/lua/plugins/languages.lua
+++ nvim/lua/plugins/languages.lua
@@ -5,16 +5,45 @@
 local plugins = {
 
   -- Treesitter
-  {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"},
+  {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate",
+    opts = {
+      ensure_installed = { 
+        'markdown', 
+        'markdown_inline',
+        'html',      -- add these if you want them
+        'latex',
+        'yaml',
+      },
+      highlight = {
+        enable = true,
+      },
+    },
+  },
 
   -- Markdown
   {
     'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },            -- if you use the mini.nvim suite
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 
+    'nvim-tree/nvim-web-devicons', -- optional, for icons
+    'nvim-mini/mini.nvim' },            -- if you use the mini.nvim suite
     opts = {
       heading = { sign = false },
+      html = { enabled = false },
+      latex = { enabled = false },
+      yaml = { enabled = false },
     },
-},
+    config = function(_, opts)
+	    require('render-markdown').setup(opts)
+
+	    -- Enable treesitter highlighting for markdown
+	    vim.api.nvim_create_autocmd('FileType', {
+		    pattern = 'markdown',
+		    callback = function()
+			    vim.treesitter.start()
+		    end,
+	    })
+    end,
+  },
 
   -- SystemVerilog
   {
