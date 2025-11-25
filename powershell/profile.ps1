0a298893ecc66ac48df42905657a1553ea40aca8
Isael Diaz <Isael.Diaz@numascale.com>
2025-11-25 09:22:09 +0100

    adding Oh-my-posh instead of git-posh

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -27,17 +27,8 @@ Set-PSReadlineOption -Color @{
 }
 
 
-# Dracula Prompt Configuration
-Import-Module posh-git
-$GitPromptSettings.DefaultPromptPrefix.Text = "$([char]0x2192) " # arrow unicode symbol
-$GitPromptSettings.DefaultPromptPrefix.ForegroundColor = [ConsoleColor]::Green
-$GitPromptSettings.DefaultPromptPath.ForegroundColor =[ConsoleColor]::Cyan
-$GitPromptSettings.DefaultPromptSuffix.Text = "$([char]0x203A) " # chevron unicode symbol
-$GitPromptSettings.DefaultPromptSuffix.ForegroundColor = [ConsoleColor]::Magenta
-# Dracula Git Status Configuration
-$GitPromptSettings.BeforeStatus.ForegroundColor = [ConsoleColor]::Blue
-$GitPromptSettings.BranchColor.ForegroundColor = [ConsoleColor]::Blue
-$GitPromptSettings.AfterStatus.ForegroundColor = [ConsoleColor]::Blue
+# Oh-My-Posh Prompt Configuration
+oh-my-posh init pwsh --config "$env:USERPROFILE\.config\oh-my-posh\config.json" | Out-String | Invoke-Expression
 
 
 ########### HELPING FUNCTIONS ###############
