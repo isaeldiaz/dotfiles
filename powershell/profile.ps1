f447fb43e4be346246acf547563224d3a4bfb166
Isael Diaz <Isael.Diaz@numascale.com>
2025-11-25 13:28:41 +0100

    poweershell improvements

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -14,21 +14,13 @@ if ($env:Path -notlike "*$usersProgPath*") {
     $env:Path += ";$usersProgPath"
 }
 
-# Dracula readline configuration. Requires version 2.0, if you have 1.2 convert to `Set-PSReadlineOption -TokenType`
-Set-PSReadlineOption -Color @{
-    "Command" = [ConsoleColor]::Green
-    "Parameter" = [ConsoleColor]::Gray
-    "Operator" = [ConsoleColor]::Magenta
-    "Variable" = [ConsoleColor]::White
-    "String" = [ConsoleColor]::Yellow
-    "Number" = [ConsoleColor]::Blue
-    "Type" = [ConsoleColor]::Cyan
-    "Comment" = [ConsoleColor]::DarkCyan
+# Oh-My-Posh Prompt Configuration (Material Theme)
+$configPath = "$env:USERPROFILE\.config\oh-my-posh\config.json"
+if (Test-Path $configPath) {
+    oh-my-posh init pwsh --config $configPath | Out-String | Invoke-Expression
+} else {
+    Write-Warning "Oh-My-Posh config not found at $configPath"
 }
-
-
-# Oh-My-Posh Prompt Configuration
-oh-my-posh init pwsh --config "$env:USERPROFILE\.config\oh-my-posh\config.json" | Out-String | Invoke-Expression
 
 
 ########### HELPING FUNCTIONS ###############
