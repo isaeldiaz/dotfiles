f1910f3efe88517d31e3f5cb8d483f445182f6ec
Isael Diaz <Isael.Diaz@numascale.com>
2026-05-27 10:10:18 +0200

    hot fix to tmux auto attach configuration in bash

--- scripts/tmux-auto-attach.sh
+++ scripts/tmux-auto-attach.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 [[ -n "$TMUX" ]] && exit 0          # already inside tmux
-[[ $- != *i* ]] && exit 0           # non-interactive shell
 
 if tmux has-session 2>/dev/null; then
   exec tmux attach
